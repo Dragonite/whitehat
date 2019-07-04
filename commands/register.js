314a0a6ccd6bd0a2569c463b726e2a23d833a5b4
Dragonite <haolin97@gmail.com>
2019-07-04 16:39:13 +0800

    Add register command and add colors from botconfig file

--- commands/register.js
+++ commands/register.js
@@ -1,14 +1,26 @@
 const config = require("../botconfig.json");
 let connection = require('../sql');
-const adminList = config.adminList;
+const { colors } = config;
 
-exports.run = (client, message, args, clientEventChannel, everyone, getAdminName) => {
+exports.run = (client, message, args) => {
+
+  async function createUser(id, full_name, description, link) {
+    let query = `INSERT IGNORE INTO users (discord_id, full_name, description, link) VALUES ("${id}", "${full_name}", "${description}", "${link}")`;
+    let promise = new Promise((resolve, reject) => {
+      connection.query(query, function (err, result) {
+        if (err) throw err;
+        resolve(result);
+      })
+    });
+    return promise;
+  }
+
   const newLineArgs = message.content.slice(config.prefix.length).trim().split('\n');
   newLineArgs.shift();
   if (newLineArgs.length === 0) {
     message.channel.send({
       embed: {
-        color: 3447003,
+        color: colors.info,
         title: "Register your Profile",
         description: "It's often hard to know who's who on Discord, so please register yourself in the following format:\n```!register\nFull Name\nDescription (Short description about yourself)\nLink (Github, LinkedIn, etc)```",
         footer: {
@@ -18,6 +30,57 @@ exports.run = (client, message, args, clientEventChannel, everyone, getAdminName
       }
     })
 
+  } else if (newLineArgs.length == 2 || newLineArgs.length == 3) {
+    const full_name = newLineArgs[0];
+    const description = newLineArgs[1];
+    const link = newLineArgs[2] ? newLineArgs[2] : "No link provided";
+    createUser(message.author.id, full_name, description, link).then((result) => {
+      if (result === null || result === undefined || result.affectedRows === 0) {
+        message.channel.send(
+          {
+            embed: {
+              color: colors.warning,
+              title: 'Profile already exists!',
+              description: 'You already have a profile. Use **!profile** to view it',
+              timestamp: new Date(),
+              footer: {
+                icon_url: client.user.avatarURL,
+                text: 'Bot'
+              }
+            }
+          }
+        )
+      } else {
+        message.channel.send(
+          {
+            embed: {
+              color: colors.success,
+              title: "Profile created successfully!",
+              timestamp: new Date(),
+              footer: {
+                icon_url: message.author.avatarURL,
+                text: message.author.username
+              }
+            }
+          }
+        )
+      }
+    })
+  } else {
+    message.channel.send(
+      {
+        embed: {
+          color: colors.danger,
+          title: 'Invalid Command Structure',
+          description: "Please check the structure to register a profile by typing **!register**, otherwise if you have a profile already, type **!profile**",
+          timestamp: new Date(),
+          footer: {
+            icon_url: client.user.avatarURL,
+            text: 'Bot'
+          }
+        }
+      }
+    )
   }
 }
 
