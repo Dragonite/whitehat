f4202a07a7cf0cea42fd747da5164702733fdb91
Dragonite <haolin97@gmail.com>
2019-07-04 16:38:17 +0800

    Add connection timeout error checks for MySQL

--- sql.js
+++ sql.js
@@ -2,13 +2,28 @@ const mysql = require('mysql');
 const config = require("./botconfig.json");
 const { host, user, database } = config.database;
 
-let connection = mysql.createConnection({ host, user, database });
+let connection;
 
-connection.connect(function (err) {
-  if (err) {
-    throw err;
-  };
-  console.log("MySQL Connected!");
-});
+function handleDisconnect() {
+  connection = mysql.createConnection({ host, user, database });
+
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+    console.log('MySQL Connected!')
+  });
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports=connection;
