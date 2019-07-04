3d4152ede9b77735dec186cf6facc12f22f452cc
Dragonite <haolin97@gmail.com>
2019-07-04 16:55:49 +0800

    Update post with colors from config, add check for multi args

--- commands/post.js
+++ commands/post.js
@@ -1,12 +1,12 @@
 const config = require("../botconfig.json");
-const { adminList, welcomeChannel } = config;
+const { adminList, welcomeChannel, colors } = config;
 
 exports.run = async (client, message, args) => {
   if (adminList.includes(message.author.id)) {
     if (args.length === 0) {
       message.channel.send({
         embed: {
-          color: 3447003,
+          color: colors.info,
           title: "Create a Post",
           description: `**!post** takes one *parameter*. <@${client.user.id}> is able to generate static messages in channels based on this *parameter*.\n\nUsage: **!post parameter**\n\nAvailable Parameters:`,
           fields: [
@@ -26,7 +26,7 @@ exports.run = async (client, message, args) => {
         case 'role':
           let msg = await message.channel.send({
             embed: {
-              color: 3447003,
+              color: colors.info,
               title: "Welcome to the UWA Ethical Hacking Club Discord!",
               description: "This Discord Server will be used to communicate upcoming events, as well as provide a platform for discussion for the club.\n\n If you would like to help other students with Hack The Box, Linux, or anything else, please react to this post to receive a ***Helper Role*** that students can ping you with.",
               fields: [
@@ -56,7 +56,7 @@ exports.run = async (client, message, args) => {
           message.channel.send(
             {
               embed: {
-                color: 3447003,
+                color: colors.danger,
                 title: "**!post** *parameter* does not exist!",
                 footer: {
                   icon_url: client.user.avatarURL,
@@ -67,6 +67,21 @@ exports.run = async (client, message, args) => {
           )
 
       }
+    } else {
+      message.channel.send(
+        {
+          embed: {
+            color: colors.danger,
+            title: 'Invalid Command Structure',
+            description: "Please check the posts available by typing **!post**",
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: 'Bot'
+            }
+          }
+        }
+      )
     }
   } else {
     message.channel.send("You must be an Administrator to use the **!post** command.");
